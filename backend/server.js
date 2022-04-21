4fcf892819427887239e836fd412af2a8691c5ce
shyarnis <shyarnisgg88@gmail.com>
2022-04-21 18:58:19 +0545

    Connect to the database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 require('dotenv').config()
 const express = require("express")
 const app = express()
+const mongoose = require("mongoose")
 
 const PORT = process.env.PORT
 const HOSTNAME = process.env.HOSTNAME
+
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => { console.log("Connected to the Database")})
 
 app.listen(PORT, () => {
 	console.log(`Server is started at http://${HOSTNAME}:${PORT}`)
